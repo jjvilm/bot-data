7372baf59582b4e062718cdc6ccf87eb66c264f2
Nik Spencer <123907213+spenik1711@users.noreply.github.com>
2023-04-12 23:31:05 +0000

    Made user creation page.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('usercreate', { title: 'Express' });
   // res.send('This will be main page: /login page');
 });
 
