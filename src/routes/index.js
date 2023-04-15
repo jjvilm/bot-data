b53f94f0a931e43828661b1e3be3c3d160c4d623
Nik Spencer <123907213+spenik1711@users.noreply.github.com>
2023-04-15 18:01:02 +0000

    Added patient list, edit and create, user create, and edit.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('usercreate', { title: 'Express' });
+  res.render('patientlist', { title: 'Express' });
   // res.send('This will be main page: /login page');
 });
 
