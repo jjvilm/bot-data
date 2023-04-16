7cbf9bd73ccb22d0cc5deac826a4102f15ea25d3
QuXiangjie <108599231+QuXiangjie@users.noreply.github.com>
2023-04-16 21:50:12 +0000

    update the usercreate.ejs and css create the userController.js create the config folder and create the user.js for local login install a bunch of packages

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/userController'); 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -7,5 +8,9 @@ router.get('/', function (req, res, next) {
   res.render('login', { title: 'Express' });
   // res.send('This will be main page: /login page');
 });
+router.get('/usercreate', function (req, res, next) {
+  res.render('usercreate', { title: 'Express' });
+});
+router.post('/usercreate', userController.createUser);
 
 module.exports = router;
